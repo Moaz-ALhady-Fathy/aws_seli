76c41bbaf625b1084cc32fd7d40f1879ad9fd6a7
Moaz-ALhady-Fathy <75454774+Moaz-ALhady-Fathy@users.noreply.github.com>
2023-04-01 22:14:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.headless = True
-wd = webdriver.Chrome('/content/chromedriver',options=chrome_options)
+wd = webdriver.Chrome('chromedriver',options=chrome_options)
 wd.get("https://www.classcentral.com/")
 print(wd.page_source)
